226d864e062fa679db240ddb7a1650c55132a8b7
machakux <bmachaku@gmail.com>
2014-08-26 18:44:11 +0300

    Adding delete_facilities and delete_services commands.

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from pprint import pprint
 
 from flask.ext.script import Manager
 
-from taarifa_api import add_document
+from taarifa_api import add_document, delete_documents
 
 from taarifa_schools import app
 from taarifa_schools.schemas import facility_schema, service_schema
@@ -31,5 +31,19 @@ def create_service():
     """Register project service."""
     check(add_document('services', service_schema))
 
+
+@manager.command
+def delete_facilities():
+    """Delete all facilities."""
+    check(delete_documents('facilities'), 200)
+
+
+@manager.command
+def delete_services():
+    """Delete all services."""
+    check(delete_documents('services'), 200)
+
+
 if __name__ == "__main__":
     manager.run()
+
